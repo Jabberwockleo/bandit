1d4315c0bf249fe7727af1c3dee697c44767e954
leowan <jabberwockwanli@163.com>
2018-12-09 22:38:09 +0800

    dev

---
+++ src/linUCB.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+#
+# Copyright (c) 2018 Wan Li. All Rights Reserved
+#
+########################################################################
+
+"""
+File: linUCB.py
+Author: leowan(leowan)
+Date: 2018/12/7 16:14:36
+
+Reference:
+    A Contextual-Bandit Approach to Personalized News Article Recommendation
+    https://arxiv.org/abs/1003.0146
+"""
+
---
+++ src/thompson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+#
+# Copyright (c) 2018 Wan Li. All Rights Reserved
+#
+########################################################################
+
+"""
+File: thompson.py
+Author: leowan(leowan)
+Date: 2018/12/7 16:14:36
+
+Reference:
+    A Tutorial on Thompson Sampling
+    https://web.stanford.edu/~bvr/pubs/TS_Tutorial.pdf
+    
+    Beta Distribution
+    https://en.wikipedia.org/wiki/Beta_distribution
+"""
+
